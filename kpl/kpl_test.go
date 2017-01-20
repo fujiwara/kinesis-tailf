0f292e87978a7af8fb8c620ad3d60622fea36150
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2017-01-20 16:20:12 +0900

    add tests

--- kpl/kpl_test.go
+++ kpl/kpl_test.go
@@ -8,23 +8,25 @@ import (
 	"github.com/fujiwara/kinesis-tailf/kpl"
 )
 
-func TestRoundTrip(t *testing.T) {
-	ar := &kpl.AggregatedRecord{
-		PartitionKeyTable:    []string{"foo", "bar"},
-		ExplicitHashKeyTable: []string{"foo", "bar"},
-		Records: []*kpl.Record{
-			&kpl.Record{
-				PartitionKeyIndex:    Uint64(1),
-				ExplicitHashKeyIndex: Uint64(2),
-				Data:                 []byte("data1"),
-			},
-			&kpl.Record{
-				PartitionKeyIndex:    Uint64(3),
-				ExplicitHashKeyIndex: Uint64(4),
-				Data:                 []byte("data2"),
-			},
+var ar = &kpl.AggregatedRecord{
+	PartitionKeyTable:    []string{"foo", "bar"},
+	ExplicitHashKeyTable: []string{"foo", "bar"},
+	Records: []*kpl.Record{
+		&kpl.Record{
+			PartitionKeyIndex:    Uint64(1),
+			ExplicitHashKeyIndex: Uint64(2),
+			Data:                 []byte("data1"),
 		},
-	}
+		&kpl.Record{
+			PartitionKeyIndex:    Uint64(3),
+			ExplicitHashKeyIndex: Uint64(4),
+			Data:                 []byte("data2"),
+		},
+	},
+}
+var serialized, _ = kpl.Marshal(ar)
+
+func TestRoundTrip(t *testing.T) {
 	b, err := kpl.Marshal(ar)
 	if err != nil {
 		t.Error(err)
@@ -37,6 +39,24 @@ func TestRoundTrip(t *testing.T) {
 	j2, _ := json.Marshal(ar2)
 	if !bytes.Equal(j1, j2) {
 		t.Error("roudtrip failed %s %s", string(j1), string(j2))
+	}
+}
+
+func BenchmarkMarshal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		kpl.Marshal(ar)
+	}
+}
+
+func BenchmarkUnmarshal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		kpl.Unmarshal(serialized)
+	}
+}
+
+func BenchmarkMarshalJSON(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		json.Marshal(ar)
 	}
 }
 
